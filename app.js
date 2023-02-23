791271f83c6f69ec5a20c21a552894b342a78ba2
Switch <32131859+SwitchGM@users.noreply.github.com>
2023-02-23 13:07:58 +0000

    added example image

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT | 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + "/example-image.png")
 })
 
 app.listen(port, () => {
