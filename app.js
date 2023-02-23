23d0671a7ba39443d844590e1561d29c1546328c
Switch <32131859+SwitchGM@users.noreply.github.com>
2023-02-23 14:31:09 +0000

    changed port setting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { createCanvas } = require("canvas")
 const app = express()
-const port = process.env.PORT | 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
