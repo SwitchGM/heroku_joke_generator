cfeff8e3c1055cb8bcf5c7ac95180726f39bd9f9
Switch <32131859+SwitchGM@users.noreply.github.com>
2023-02-23 17:20:37 +0000

    basic example of joke image

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const { createCanvas } = require("canvas")
+const path = require("path")
+const Canvas = require("canvas")
 const app = express()
 const port = process.env.PORT || 5000
+
+const { jokes } = require("./jokes.json")
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
@@ -21,10 +24,37 @@ app.get('/', (req, res) => {
     res.end(img)
 })
 
+function fontFile (name) {
+    return path.join(__dirname, '/fonts/', name)
+}
+
+function randomInt(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 function createImage() {
-    c = createCanvas(100, 100)
-    ctxt = c.getContext("2d")
-    ctxt.fillStye = "#123123"
-    ctxt.fillRect(0, 0, 50, 50)
+    Canvas.registerFont(fontFile("Calibri Regular.ttf"), {family: "Calibri"})
+    
+    let joke = jokes[randomInt(0, jokes.length - 1)] 
+    
+    c = Canvas.createCanvas(9999, 100)
+    ctx = c.getContext("2d")
+
+    jokeWidth = ctx.measureText(joke).width
+    jokeHeight = ctx.measureText(joke).height
+
+    tmp = parseInt(jokeWidth * 1.1)
+
+    console.log(jokeWidth, tmp, jokeHeight, joke)
+
+
+    c.width = parseInt(jokeWidth * 3.1)
+    // c.height = parseInt(jokeHeight * 1.1)
+
+    ctx.fillStyle = "black"
+    ctx.textAlign = "center"
+    ctx.font = "24pt Calibri"
+    ctx.fillText(joke, c.width / 2, c.height / 2)
+
     return c.toBuffer("image/png")
 }
