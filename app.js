8b625d64aeca32cd0d1842050f1f4e01404605b0
Switch <32131859+SwitchGM@users.noreply.github.com>
2023-02-23 13:22:37 +0000

    generate basic image

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const { createCanvas } = require("canvas")
 const app = express()
 const port = process.env.PORT | 5000
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/example-image.png")
+//   res.sendFile(__dirname + "/example-image.png")
+    data = createImage()
+    img = Buffer.from(data, "base64")
+    res.writeHead(
+        200, {
+            "Content-Type": "image/png",
+            "Context-Length": img.length
+        }
+    )
+    res.end(img)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function createImage() {
+    c = createCanvas(100, 100)
+    ctxt = c.getContext("2d")
+    ctxt.fillStye = "#123123"
+    ctxt.fillRect(0, 0, 50, 50)
+    return c.toBuffer("image/png")
+}
