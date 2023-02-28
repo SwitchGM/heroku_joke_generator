83c34a2a06a9313a05f2eb4d0036a34fd84f0537
Switch <32131859+SwitchGM@users.noreply.github.com>
2023-02-28 11:57:53 +0000

    changes to image and text sizes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path")
 const Canvas = require("canvas")
 const app = express()
 const port = process.env.PORT || 5000
+const px = 11;
 
 const { jokes } = require("./jokes.json")
 
@@ -37,24 +38,13 @@ function createImage() {
     
     let joke = jokes[randomInt(0, jokes.length - 1)] 
     
-    c = Canvas.createCanvas(9999, 100)
+    c = Canvas.createCanvas(1000, px*1.3)
     ctx = c.getContext("2d")
 
-    jokeWidth = ctx.measureText(joke).width
-    jokeHeight = ctx.measureText(joke).height
-
-    tmp = parseInt(jokeWidth * 1.1)
-
-    console.log(jokeWidth, tmp, jokeHeight, joke)
-
-
-    c.width = parseInt(jokeWidth * 3.1)
-    // c.height = parseInt(jokeHeight * 1.1)
-
     ctx.fillStyle = "black"
-    ctx.textAlign = "center"
-    ctx.font = "24pt Calibri"
-    ctx.fillText(joke, c.width / 2, c.height / 2)
+    // ctx.textAlign = "center"
+    ctx.font = `${px}pt Calibri`
+    ctx.fillText(joke, 0, px)
 
     return c.toBuffer("image/png")
 }
